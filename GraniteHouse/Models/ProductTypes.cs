e4fcae41cc87b277e5694e017fc8cb2ef5c02163
Lucy Quantick <lucyquantick@gmail.com>
2019-06-19 15:36:42 +0100

    Create DB and added ProductType model

---
+++ GraniteHouse/Models/ProductTypes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Models
+{
+	public class ProductTypes
+	{
+		// This Id is a primary key so is [Required] by default, it will be generated automatically by the DB
+		public int Id { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+	}
+}
