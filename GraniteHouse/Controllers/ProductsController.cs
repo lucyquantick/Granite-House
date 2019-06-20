03a35c7e9d4d227bf0e36997d5a81d0ce5e41fba
Lucy Quantick <lucyquantick@gmail.com>
2019-06-20 12:11:18 +0100

    Add Create action method and default image

--- GraniteHouse/Controllers/ProductsController.cs
+++ GraniteHouse/Controllers/ProductsController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using GraniteHouse.Data;
 using GraniteHouse.Models.ViewModel;
+using GraniteHouse.Utility;
+using Microsoft.AspNetCore.Hosting.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,14 +18,16 @@ namespace GraniteHouse.Controllers
 	{
 
 		private readonly ApplicationDbContext _db;
+		private readonly HostingEnvironment _hostingEnvironment;
 
 		[BindProperty]
-		public ProductsViewModel ProductsVm { get; set; }
+		public ProductsViewModel ProductsVM { get; set; }
 
-		public ProductsController(ApplicationDbContext db)
+		public ProductsController(ApplicationDbContext db, HostingEnvironment hostingEnvironment)
 		{
 			_db = db;
-			ProductsVm = new ProductsViewModel()
+			_hostingEnvironment = hostingEnvironment;
+			ProductsVM = new ProductsViewModel()
 			{
 				ProductTypes = _db.ProductTypes.ToList(),
 				SpecialTags = _db.SpecialTags.ToList(),
@@ -30,13 +35,69 @@ namespace GraniteHouse.Controllers
 			};
 		}
 
-		// GET index
+		// Index
 		public async Task<IActionResult> Index()
 		{
 			var products = _db.Products.Include(mbox => mbox.ProductTypes).Include(mbox => mbox.SpecialTags);
 
 			return View(await products.ToListAsync());
 		}
+
+		// Get : Products Create
+		public IActionResult Create()
+		{
+			return View(ProductsVM);
+		}
+
+		// Post : Products Create
+		[HttpPost,ActionName("Create")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> CreatePOST()
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(ProductsVM);
+			}
+
+			_db.Products.Add(ProductsVM.Products);
+			await _db.SaveChangesAsync();
+
+			// Image being saved
+
+			string webRootPath = _hostingEnvironment.WebRootPath;
+			var files = HttpContext.Request.Form.Files;
+
+			var productsFromDb = _db.Products.Find(ProductsVM.Products.Id);
+
+			if (files.Count != 0)
+			{
+				// Image has been uploaded
+				var uploads = Path.Combine(webRootPath, SD.ImageFolder);
+				var extension = Path.GetExtension(files[0].FileName);
+
+				// copy file uploaded to the server
+				using (var filestream = new FileStream(Path.Combine(uploads, ProductsVM.Products.Id + extension), FileMode.Create))
+				{
+					files[0].CopyTo(filestream);
+				}
+				productsFromDb.Image = @"\" + SD.ImageFolder + @"\" + ProductsVM.Products.Id + extension;
+
+			}
+			else
+			{
+				// When user does not upload image
+				var uploads = Path.Combine(webRootPath, SD.ImageFolder + @"\" + SD.DefaultProductImage);
+
+				System.IO.File.Copy(uploads, webRootPath + @"\" + SD.ImageFolder + @"\" + ProductsVM.Products.Id + ".jpg");
+
+				productsFromDb.Image = @"\" + SD.ImageFolder + @"\" + ProductsVM.Products.Id + ".jpg";
+			}
+
+			await _db.SaveChangesAsync();
+
+			return RedirectToAction(nameof(Index));
+		}
+
 
 	}
 }
