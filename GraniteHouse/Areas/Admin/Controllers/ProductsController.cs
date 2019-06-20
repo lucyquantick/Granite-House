69304c5c6d7b702ddb74c839832423453e6d4800
Lucy Quantick <lucyquantick@gmail.com>
2019-06-20 16:41:44 +0100

    Added Delete action method and view

--- GraniteHouse/Areas/Admin/Controllers/ProductsController.cs
+++ GraniteHouse/Areas/Admin/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using GraniteHouse.Data;
+using GraniteHouse.Models;
 using GraniteHouse.Models.ViewModel;
 using GraniteHouse.Utility;
 using Microsoft.AspNetCore.Hosting.Internal;
@@ -184,6 +185,52 @@ namespace GraniteHouse.Controllers
 				return NotFound();
 			}
 			return View(ProductsVM);
+		}
+
+		// GET : Delete
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			ProductsVM.Products = await _db.Products.Include(m => m.SpecialTags).Include(m => m.ProductTypes).SingleOrDefaultAsync(m => m.Id == id);
+
+			if (ProductsVM.Products == null)
+			{
+				return NotFound();
+			}
+			return View(ProductsVM);
+		}
+
+		// POST : Delete
+		[HttpPost,ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			string webRootPath = _hostingEnvironment.WebRootPath;
+			Products products = await _db.Products.FindAsync(id);
+
+			if(products == null)
+			{
+				return NotFound();
+			}
+			else
+			{
+				var uploads = Path.Combine(webRootPath, SD.ImageFolder);
+				var extension = Path.GetExtension(products.Image);
+
+				if (System.IO.File.Exists(Path.Combine(uploads, products.Id + extension)))
+				{
+					System.IO.File.Delete(Path.Combine(uploads, products.Id + extension));
+				}
+				_db.Products.Remove(products);
+
+				await _db.SaveChangesAsync();
+
+				return RedirectToAction(nameof(Index));
+			}
 		}
 
 	}
