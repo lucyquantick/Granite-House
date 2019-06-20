2e4e6f294bcee5d77beef92a5a573b64be24b412
Lucy Quantick <lucyquantick@gmail.com>
2019-06-20 10:04:57 +0100

    Add Products model to database, add products controller and index method

---
+++ GraniteHouse/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteHouse.Data;
+using GraniteHouse.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraniteHouse.Controllers
+{
+	public class ProductsController : Controller
+	{
+
+		private readonly ApplicationDbContext _db;
+
+		[BindProperty]
+		public ProductsViewModel ProductsVm { get; set; }
+
+		public ProductsController(ApplicationDbContext db)
+		{
+			_db = db;
+			ProductsVm = new ProductsViewModel()
+			{
+				ProductTypes = _db.ProductTypes.ToList(),
+				SpecialTags = _db.SpecialTags.ToList(),
+				Products = new Models.Products()
+			};
+		}
+
+		// GET index
+		public async Task<IActionResult> Index()
+		{
+			var products = _db.Products.Include(mbox => mbox.ProductTypes).Include(mbox => mbox.SpecialTags);
+
+			return View(await products.ToListAsync());
+		}
+
+	}
+}
