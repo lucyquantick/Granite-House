490049e39ee35ae707af2fe288bc49afeb2528dc
Lucy Quantick <lucyquantick@gmail.com>
2019-07-02 16:35:05 +0100

    register post handler

--- GraniteHouse/obj/Debug/netcoreapp2.1/Razor/Areas/Customer/Views/Home/Index.g.cshtml.cs
+++ GraniteHouse/obj/Debug/netcoreapp2.1/Razor/Areas/Customer/Views/Home/Index.g.cshtml.cs
@@ -95,7 +95,7 @@ WriteAttributeValue("", 401, product.Image, 401, 14, false);
             WriteLiteral("</b>/sq.ft</label>\r\n\t\t\t\t\t</div>\r\n\t\t\t\t\t");
             EndContext();
             BeginContext(644, 141, false);
-            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("a", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "011e99e0bceb4a7b8fd04202c50715fc", async() => {
+            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("a", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "8cab2ad2ee124828b3eee3bbf723321e", async() => {
                 BeginContext(769, 12, true);
                 WriteLiteral("View Details");
                 EndContext();
