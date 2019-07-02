162693ed44862f38bef44654417066a8193ed81e
Lucy Quantick <lucyquantick@gmail.com>
2019-07-02 10:44:41 +0100

    Add remove from bag button

--- GraniteHouse/obj/Debug/netcoreapp2.1/Razor/Areas/Customer/Views/Home/Index.g.cshtml.cs
+++ GraniteHouse/obj/Debug/netcoreapp2.1/Razor/Areas/Customer/Views/Home/Index.g.cshtml.cs
@@ -95,7 +95,7 @@ WriteAttributeValue("", 401, product.Image, 401, 14, false);
             WriteLiteral("</b>/sq.ft</label>\r\n\t\t\t\t\t</div>\r\n\t\t\t\t\t");
             EndContext();
             BeginContext(644, 141, false);
-            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("a", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "fb3834695d2541c7a6cef8b0d5310fb8", async() => {
+            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("a", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "1ceda56889a24918965f31383b771b33", async() => {
                 BeginContext(769, 12, true);
                 WriteLiteral("View Details");
                 EndContext();
