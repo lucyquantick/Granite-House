7115e2f7cce7a183a48870e9cc799efbc547effe
Lucy Quantick <lucyquantick@gmail.com>
2019-07-02 16:06:59 +0100

    Application User added to Db

--- GraniteHouse/Data/ApplicationDbContext.cs
+++ GraniteHouse/Data/ApplicationDbContext.cs
@@ -20,5 +20,7 @@ namespace GraniteHouse.Data
 
 		public DbSet<Appointments> Appointments { get; set; }
 		public DbSet<ProductsSelectedForAppointment> productsSelectedForAppointments { get; set; }
+
+		public DbSet<ApplicationUser> ApplicationUser { get; set; }
 	}
 }
